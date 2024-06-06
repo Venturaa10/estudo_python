5301232843484dc765ba14566419b9fb2cd313ca
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-05 21:26:04 -0300

    Protegendo atributos importantes, acessando e modificando de forma controlado usando o property e  .setter

--- python_POO/modelo.py
+++ python_POO/modelo.py
@@ -2,26 +2,59 @@
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        '''Metodo construtor'''
-        self.nome = nome.title()
+        '''Metodo construtor
+        O dulplo underline é uma forma de proteger o atributo
+        '''
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        '''O decorador @property transforma o método likes em uma propriedade, permitindo que ele seja acessado como se fosse um atributo, de uma forma controlada'''
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        '''Quando esse atributo é chamado, é acrescentado 1 like ao filme / serie'''
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        '''O decorador @nome.setter define um setter para a propriedade nome, permitindo que ela seja modificada de fora da classe '''
+    
+        self.__nome = novo_nome.title()
+
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
         '''Metodo construtor'''
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
 
     def dar_like(self):
-        self.likes += 1
+        '''Quando esse atributo é chamado, é acrescentado 1 like ao filme / serie'''
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('vingadores - guerra infinita', 2020, 160)
