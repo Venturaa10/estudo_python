931a360e5761494ebcb232565d8ceb579094048b
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-05 21:49:35 -0300

    Removi duplicação, criei e adicionei a 'Pai'

--- python_POO/modelo.py
+++ python_POO/modelo.py
@@ -1,60 +1,51 @@
 # 'modelo' é o nome dado geralmente ao arquivo que irei trabalhar com classes
 
-class Filme:
-    def __init__(self, nome, ano, duracao):
+class Programa:
+    def __init__(self, nome, ano):
         '''Metodo construtor
-        O dulplo underline é uma forma de proteger o atributo
+        Um underline é usado como convenção, apenas para dizer ao outro programador que o atributo não deve ser acessado diretamente fora da classe
+        O dulplo underline é uma forma de proteger o atributo de fato, tornando mais dificil o acesso
         '''
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def likes(self):
         '''O decorador @property transforma o método likes em uma propriedade, permitindo que ele seja acessado como se fosse um atributo, de uma forma controlada'''
-        return self.__likes
+        return self._likes
 
     def dar_like(self):
         '''Quando esse atributo é chamado, é acrescentado 1 like ao filme / serie'''
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome(self, novo_nome):
         '''O decorador @nome.setter define um setter para a propriedade nome, permitindo que ela seja modificada de fora da classe '''
     
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
 
+class Filme(Programa):
+    '''Classe Filme está herdando da classe Programa'''
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self._likes = 0
 
-class Serie:
+
+class Serie(Programa):
+    '''Classe Serie está herdando da classe Programa'''
     def __init__(self, nome, ano, temporadas):
-        '''Metodo construtor'''
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-
-    def dar_like(self):
-        '''Quando esse atributo é chamado, é acrescentado 1 like ao filme / serie'''
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._likes = 0
 
 
 vingadores = Filme('vingadores - guerra infinita', 2020, 160)
