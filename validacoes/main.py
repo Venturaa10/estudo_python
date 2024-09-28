aabe48cf776a859906b7c9747e0cac9745add84e
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-09-28 15:29:48 -0300

    Refatorando o código de validação de documento

--- validacoes/main.py
+++ validacoes/main.py
@@ -1,4 +1,4 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import Documento
 import os
 os.system('cls')
 
@@ -11,10 +11,13 @@ print(cpf.validate('01234567890')) # Retorna True
 print(cpf.validate('23345623345')) # Retorna False
 '''
 
-cpf1 = CpfCnpj('15316264754', 'cpf')
+cpf1 = Documento.cria_documento('15316264754')
 print(cpf1)
-# cpf2 = Cpf('11111111122')
+# cpf2 = Documento.cria_documento('11111111111')
 # print(cpf2)
 
-cnpj1 = CpfCnpj('35379838000112', 'cnpj')
+
+cnpj1 = Documento.cria_documento('35379838000112')
 print(cnpj1)
+# cnpj2 = Documento.cria_documento('11111111111111')
+# print(cnpj2)
