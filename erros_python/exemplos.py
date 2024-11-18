763290f5b672e37c48e15565a080b44739489ec8
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-11-18 11:01:39 -0300

    Adicionar metodo setter privado para verificação do valor dos atributos da classe, antes de atribuir um valor ao mesmo.

--- erros_python/exemplos.py
+++ erros_python/exemplos.py
@@ -1,7 +1,13 @@
 def dividir(dividendo, divisor):
-    # print(divisor.resultado)
-    return dividendo / divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)): # Se dividendo e divisor não forem valores inteiros, retorna um raise ValueError
+        raise ValueError('dividir() deve receber apenas argumentos inteiros')
+    try:
+        aux = dividendo / divisor
+    except:
+        print(f'Não foi possivel dividir {dividendo} por {divisor}')
+        raise 
 
+    return aux
 
 def testa_divisao(divisor):
     resultado = dividir(10, divisor)
@@ -11,15 +17,16 @@ def testa_divisao(divisor):
 
 # testa_divisao(2)
 try:
-    testa_divisao(0)
-
-except Exception as E:
-    print('Tratamento Generico!')
+    testa_divisao(2.5)
 
 except ZeroDivisionError as E:
     print('Erro de divisão por zero!')
 
+except Exception as E:
+    print('Tratamento Generico!')
+
 print('Programa encerrado')
+
 
 
 # os blocos except são visitados de cima para baixo e nenhum bloco except após o except (Exception) será executado, uma vez que este captura todas as exceções do Python, ou seja, as exceções do ZeroDivisionError não serão capturados.
--- erros_python/main.py
+++ erros_python/main.py
@@ -11,12 +11,51 @@ class ContaCorrente:
     taxa_operacao = None
 
     def __init__(self, cliente, agencia, numero):
-        self.saldo = 100
-        self.cliente = cliente
-        self.agencia = agencia
-        self.numero = numero
+        # É uma boa pratica criar todos os atributos da classe, dentro de seu inicializador.
+        self.__saldo = 100
+        self.cliente = cliente        
+        self.__set_agencia(agencia) # Verifica valor de agencia atraves do setter.
+        self.__numero = numero
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = 30 / ContaCorrente.total_contas_criadas
+
+    @property
+    def agencia(self):
+        return self.__agencia
+    
+    def __set_agencia(self, value): 
+        ''' Metodo setter privado para o atributo "agencia", responsavel por realizar as verificações necessarias antes de atribuir um valor ao mesmo'''
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            print('O atributo agencia deve ser maior que zero')
+            return
+        
+        self.__agencia = value
+
+    @property
+    def numero(self):
+        return self.__numero
+    
+    def __set_numero(self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+        
+        self.__numero = value
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @saldo.setter
+    def saldo(self, value):
+        if not isinstance(value, int):
+            return
+        if value <= 0:
+            return
+        self.__saldo = value
 
     def transferir(self, valor, favorecido):
         favorecido.depositar(valor)
@@ -28,12 +67,12 @@ class ContaCorrente:
         self.saldo += valor
 
 
-conta_corrente1 = ContaCorrente(None, '00', '101')
-
+conta_corrente1 = ContaCorrente(None, 'agencia falsa', '22500')
+conta_corrente1.agencia = 0
+print(conta_corrente1.agencia)
 
 cliente1 = Cliente('João Victor', '11122233345', 'Engenheiro de Software')
-# print(cliente1.nome)
 cliente1.idade = 20
-print(cliente1.idade)
+print(f'\n{cliente1.idade}')
 pprint(cliente1.__dict__, width=40) # Os atributos do objeto são exibidos na sixtaxe de um dicionario.
 
